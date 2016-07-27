0f7318f477b4ed99b8845497ca4dbe1915149f8a
TonyJin99 <tony99.jin@gmail.com>
2016-07-27 15:17:57 -0400

    Initialize

---
+++ Shopping/Shopping/Bridge.h
@@ -0,0 +1,10 @@
+//
+//  Bridge.h
+//  Shopping
+//
+//  Created by Tony.Jin on 7/27/16.
+//  Copyright © 2016 Innovatis Tech. All rights reserved.
+//
+
+#import "AFNetworking.h"
+
---
+++ Shopping/Shopping/TJMainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TJMainViewController.swift
+//  Shopping
+//
+//  Created by Tony.Jin on 7/27/16.
+//  Copyright © 2016 Innovatis Tech. All rights reserved.
+//
+
+import UIKit
+
+class TJMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let manager = AFHTTPSessionManager()
+        
+        let parameters = NSMutableDictionary()
+        parameters["username"] = "Michael"
+        parameters["grandTotal"] = "0"
+        
+        let url = "http://54.191.35.66:8181/pfchang/api/buy"
+        manager.POST(url, parameters: parameters, progress: { (progress) in
+            print(progress)
+            }, success: { (dataTask, object) in
+                print("sucess")
+                print(object!["rewardPoints"])
+            }) { (datatask, error) in
+                print(error)
+        }
+        
+      
+        
+
+    }
+
+}
