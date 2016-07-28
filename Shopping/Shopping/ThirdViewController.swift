577289edc69fe080003c93b26cf9fd397679156a
TonyJin99 <tony99.jin@gmail.com>
2016-07-28 12:26:48 -0400

    it is almost done

--- Shopping/Shopping/ThirdViewController.swift
+++ Shopping/Shopping/ThirdViewController.swift
@@ -9,27 +9,44 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
+    
+    let cartButton : MIBadgeButton = MIBadgeButton(frame: CGRectMake(0, 0, 40, 40))
+    var count:Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        cartButton.setImage(UIImage(named: "ios7-cart-1"), forState: .Normal)
+        let barButton : UIBarButtonItem = UIBarButtonItem(customView: cartButton)
+        self.navigationItem.rightBarButtonItem = barButton
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonAction1(sender: AnyObject) {
+        count = count + 1
+        cartButton.badgeString = "\(count)"
     }
-    */
+    
+    @IBAction func buttonAction2(sender: AnyObject) {
+        count = count + 1
+        cartButton.badgeString = "\(count)"
+    }
+    
+    
+    @IBAction func buttonAction3(sender: AnyObject) {
+        count = count + 1
+        cartButton.badgeString = "\(count)"
+    }
+    
+    @IBAction func buttonAction4(sender: AnyObject) {
+        count = count + 1
+        cartButton.badgeString = "\(count)"
+    }
+    
+    @IBAction func buttonAction5(sender: AnyObject) {
+        count = count + 1
+        cartButton.badgeString = "\(count)"
+    }
 
+    
+    
 }
