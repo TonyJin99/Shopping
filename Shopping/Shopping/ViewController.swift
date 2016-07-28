4f96ca276a34db76b5e0096d1a942b552146b6b3
TonyJin99 <tony99.jin@gmail.com>
2016-07-28 00:10:08 -0400

    已经凌晨了啊

--- Shopping/Shopping/ViewController.swift
+++ Shopping/Shopping/ViewController.swift
@@ -12,7 +12,7 @@ import LocalAuthentication
 class ViewController: UIViewController {
 
     @IBOutlet weak var coverView: UIView!
-
+    var isPresent = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,10 +44,11 @@ class ViewController: UIViewController {
             auth.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: "Secure Login", reply: {(success,policerror) -> Void in
                 
                 if success{
-                    print("login")
+//                    print("login")
+                    self.dismissViewControllerAnimated(true, completion: nil)
                     self.performSegueWithIdentifier("sunny", sender: self)
                 }else{
-                    print("falure")
+//                    print("falure")
                 }
             })
         }else{
@@ -62,24 +63,84 @@ class ViewController: UIViewController {
         let popview = UIStoryboard(name: "Pop", bundle: nil).instantiateInitialViewController()
         popview?.transitioningDelegate = self
         popview?.modalPresentationStyle = UIModalPresentationStyle.Custom
-        popview?.modalPresentationStyle = UIModalPresentationStyle.Custom
         presentViewController(popview!, animated: true, completion: nil)
         
         anthenficate()
-        
         NSNotificationCenter.defaultCenter().postNotificationName("viewChange", object: self)
  
     }
-
 }
 
 
 extension ViewController: UIViewControllerTransitioningDelegate{
     
-    func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController, sourceViewController source: UIViewController) -> UIPresentationController?
-    {
+    func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController, sourceViewController source: UIViewController) -> UIPresentationController?{
         return PresentationController(presentedViewController: presented, presentingViewController: presenting)
     }
 
+//    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning?{
+//        return self
+//    }
+//    
+//    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning?{
+//        return self
+//    }
+
 }
+
+
+//extension ViewController: UIViewControllerAnimatedTransitioning{
+//    
+//    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval{
+//        return 0.5
+//    }
+//    
+//  
+//    func animateTransition(transitionContext: UIViewControllerContextTransitioning){
+//        if isPresent{
+//            guard let toView = transitionContext.viewForKey(UITransitionContextToViewKey) else{
+//                return
+//            }
+//            
+//            transitionContext.containerView()?.addSubview(toView)
+//            
+//            toView.transform = CGAffineTransformMakeScale(1.0, 0.0)
+//            toView.layer.anchorPoint = CGPoint(x: 0.5, y: 0.0) //设置锚点
+//            UIView.animateWithDuration(transitionDuration(transitionContext), animations: {
+//                toView.transform = CGAffineTransformIdentity
+//            }) { (_) in
+//                transitionContext.completeTransition(true)
+//            }
+//        }
+//        else{
+//            guard let fromView = transitionContext.viewForKey(UITransitionContextFromViewKey) else{
+//                return
+//            }
+//            
+//            UIView.animateWithDuration(transitionDuration(transitionContext), animations: {
+//                fromView.transform = CGAffineTransformMakeScale(1.0, 0.0000001)
+//            }) { (_) in
+//                transitionContext.completeTransition(true)
+//            }
+//            
+//        }
+//        
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
